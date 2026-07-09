9920ff790a0d94e0ecae572d65b89dd601e2ecf3
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-07-09 02:24:25 +0100

    add logging to monitor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
-
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
   app.setGlobalPrefix('api');
